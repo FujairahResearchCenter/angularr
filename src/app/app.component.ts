349f685129d1a80165b5ad23a7569b1553c4b70b
Fujairah Research Center <108390870+FujairahResearchCenter@users.noreply.github.com>
2023-03-14 20:46:12 +0400

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { NgModule, Component, enableProdMode } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { DxSchedulerModule } from 'devextreme-angular';
+import { Resource, Appointment, Service } from './app.service';
+
+@Component({
+  selector: 'demo-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [Service],
+})
+export class AppComponent {
+  resources: Resource[];
+
+  appointments: Appointment[];
+
+  currentDate: Date = new Date(2021, 1, 2);
+
+  constructor(service: Service) {
+    const startDay = new Date(2021, 1, 1);
+    const endDay = new Date(2021, 1, 28);
+    const startDayHour = 8;
+    const endDayHour = 20;
+
+    this.appointments = service.generateAppointments(startDay, endDay, startDayHour, endDayHour);
+
+    this.resources = service.resources;
+  }
+}
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    DxSchedulerModule,
+  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
+
+platformBrowserDynamic().bootstrapModule(AppModule);
